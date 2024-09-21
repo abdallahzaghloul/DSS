2abca5830d0264b0bbfda29ab06a6221276d17a4
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-09-21 21:13:01 +0300

    Update DSS.py

--- DSS.py
+++ DSS.py
@@ -9,14 +9,7 @@ image= np.array(im)
 st.image(image)
 st.markdown(" <center>  <h1> Training Certificates Verification </h1> </font> </center> </h1> ",
             unsafe_allow_html=True)
-
-Well_Name = st.selectbox('The Well Name for current Testing',('BARAKAT-D01X','BARAKAT-D02X','BARAKAT-D06X','FUSTAT-N01X','IO-01X','BAT-10X','NUT-01X','SHAI-01X','ATOUN-N01X','APRIES-E01X','APRIES-E03X','ANTI-01X'))
-
-
 from datetime import time
-
-Registeration_Time = st.slider("Reading Registeration Time:", value=(time(11, 30)))
-Reading_No = st.number_input("Reading Number:",1,48,key="Reading_No" ,value=5)
 
 Date=datetime.date.today()
 Date=Date.strftime('%d-%m-%Y')
@@ -25,68 +18,29 @@ Date=Date.strftime('%d-%m-%Y')
 st.markdown(" <right>  <h1> Well Production Parameters </h1> </font> </right> </h1> ",
             unsafe_allow_html=True)
 
-CK = st.slider("C.K %", 0.00,100.00,value=30.00,key="CK",step=0.1)
-
-WHP = st.slider("WHP Psi", 300.0,5000.0,value=700.00,key="WHP",step=1.00)
-SEP_Pressure= st.slider("Separator Pressure Psi", 0.00,2000.00,value=400.00,key="SEP_Pressure",step=1.00)
-SEP_Temperature= st.slider("Separator Temp. F", 0.00,250.00,value=100.00,key="SEP_Temp",step=1.00)
-
-FLP= st.slider("FLP Psi", 0.00,1400.00,value=400.00,key="FLP",step=1.00)
-FLT = st.slider("FLT F", 0.00,250.00,value=100.00,key="FLT",step=1.00)
-
-
-Gas_Rate = st.text_input("Gas Rate (MMSCF/Day)",key="Gas_Rate")
 Condensate = st.text_input("Condensate (BPD)",key="Condensate")
-Water = st.text_input("Water (BPD)",key="Water")
-GOR = st.text_input("Gas Oil Ratio",key="GOR")
 
+#Data = {'Reading_No': Reading_No,'Well_Name': Well_Name,'Well_ID':Well_ID,'Registeration_Time':Registeration_Time,'Date':Date,'C.K%': CK,'WHP': WHP,'SEP_Pressure': SEP_Pressure,'SEP_Temperature': SEP_Temperature,'FLP': FLP,'FLT': FLT,'Gas_Rate': Gas_Rate,'Condensate': Condensate,'Water': Water,'GOR': GOR,'API': API,'BS&W': BSW,'CO2 %':CO2,'H2S':H2S,'SAL':Sal,'Remarks':Remarks}
+#df0=pd.DataFrame([Data])
+#df1=pd.DataFrame([Data])
+#df1["Date"]=pd.to_datetime(df1["Date"])
+#df1["Date"]=df1.Date.dt.strftime('%d-%m-%Y')
+#df1["C.K%"]=df1["C.K%"].astype("str")+ "%"
+#df1["BS&W"]=df1["BS&W"].astype("str")+ "%"
+#df1["Gas_Rate"]=df1["Gas_Rate"].astype("str")+ " MMSCF/D"
+#df1["Water"]=df1["Water"].astype("str")+ " BBL/D"
+#df1["Condensate"]=df1["Condensate"].astype("str")+ " BBL/D"
+#df1["WHP"]=df1["WHP"].astype("str")+ " Psi"
+#df1["SEP_Pressure"]=df1["SEP_Pressure"].astype("str")+ " Psi"
+#df1["FLP"]=df1["FLP"].astype("str")+ " Psi"
+#df1["SEP_Temperature"]=df1["SEP_Temperature"].astype("str")+ " F"
+#df1["FLT"]=df1["FLT"].astype("str")+ " F" 
+#df1["API"]=df1["API"].astype("str")+ " Deg"
 
-
-API = st.slider("API Degree", 0.00,60.00,value=32.00,key="API",step=1.00)
-BSW = st.slider("BS & W %", 0.00,100.00,value=20.00,key="BSW",step=0.1)
-
-
-st.markdown(" <center>  <h1> Well Impurities Parameters </h1> </font> </center> </h1> ",
-            unsafe_allow_html=True)
-
-CO2 = st.slider("CO2 %", 0.00,100.00,value=12.00,key="CO2",step=0.1)
-H2S = st.slider("H2S PPM", 0.00,50.00,value=8.00,key="H2S",step=0.1)
-Sal = st.slider("Salinity KPPM", 0.00,300.00,value=281.00,key="Sal",step=0.2)
-
-Remarks = st.text_input("Remarks",key="Remarks")
-
-Well_ID = Well_Name + "_" +Date
-
-
-Data = {'Reading_No': Reading_No,'Well_Name': Well_Name,'Well_ID':Well_ID,'Registeration_Time':Registeration_Time,'Date':Date,'C.K%': CK,'WHP': WHP,'SEP_Pressure': SEP_Pressure,'SEP_Temperature': SEP_Temperature,'FLP': FLP,'FLT': FLT,'Gas_Rate': Gas_Rate,'Condensate': Condensate,'Water': Water,'GOR': GOR,'API': API,'BS&W': BSW,'CO2 %':CO2,'H2S':H2S,'SAL':Sal,'Remarks':Remarks}
-df0=pd.DataFrame([Data])
-df1=pd.DataFrame([Data])
-df1["Date"]=pd.to_datetime(df1["Date"])
-df1["Date"]=df1.Date.dt.strftime('%d-%m-%Y')
-df1["C.K%"]=df1["C.K%"].astype("str")+ "%"
-df1["BS&W"]=df1["BS&W"].astype("str")+ "%"
-df1["Gas_Rate"]=df1["Gas_Rate"].astype("str")+ " MMSCF/D"
-df1["Water"]=df1["Water"].astype("str")+ " BBL/D"
-df1["Condensate"]=df1["Condensate"].astype("str")+ " BBL/D"
-df1["WHP"]=df1["WHP"].astype("str")+ " Psi"
-df1["SEP_Pressure"]=df1["SEP_Pressure"].astype("str")+ " Psi"
-df1["FLP"]=df1["FLP"].astype("str")+ " Psi"
-df1["SEP_Temperature"]=df1["SEP_Temperature"].astype("str")+ " F"
-df1["FLT"]=df1["FLT"].astype("str")+ " F" 
-df1["API"]=df1["API"].astype("str")+ " Deg"
-
-df1["CO2 %"]=df1["CO2 %"].astype("str")+ "%"
-df1["H2S"]=df1["H2S"].astype("str")+ " PPM"
-df1["SAL"]=df1["SAL"].astype("str")+ " KPPM"
+#df1["CO2 %"]=df1["CO2 %"].astype("str")+ "%"
+#df1["H2S"]=df1["H2S"].astype("str")+ " PPM"
+#df1["SAL"]=df1["SAL"].astype("str")+ " KPPM"
    
-st.dataframe(df1,width=1200)
-st.write("CO2 %  = ",CO2)
-st.write("H2S PPM  = ",H2S)
-st.write("SAL KPPM  = ",Sal)
-st.write(Remarks)
-Reading_No=str(Reading_No)
-worksheet=Well_ID+"_"+Reading_No
-sparesheet= Well_ID+"_"+str(Registeration_Time)
-
+#st.dataframe(df1,width=1200)
 col1, col2, col3 = st.columns(3)
 
